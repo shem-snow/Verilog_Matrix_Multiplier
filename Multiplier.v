0a147e259c6b59b14d265b14e324cb6e24366373
Shem <shem-snow@hotmail.com>
2024-10-31 16:11:25 -0600

    Modified the multiplier module to handle signed 2s complement numbers

--- Multiplier.v
+++ Multiplier.v
@@ -9,44 +9,20 @@ module Multiplier #(
     output reg [DATA_WIDTH+3:0] out_mat
 );
 	
+   reg [DATA_WIDTH*2-1:0] in_mat1_ext;
+    reg [DATA_WIDTH*2-1:0] in_mat2_ext;
+    reg [DATA_WIDTH*4-1:0] full_product;
 
-    // Do the multiplication blocks
-	 always@(*) begin
-		out_mat <= in_mat1 * in_mat2; 
-	 end
-	  
-endmodule
+    always @(*) begin
+        // Sign-extend `in_mat1` and `in_mat2` to 8 bits
+        in_mat1_ext = { {DATA_WIDTH{in_mat1[DATA_WIDTH-1]}}, in_mat1 };  // Extend in_mat1
+        in_mat2_ext = { {DATA_WIDTH{in_mat2[DATA_WIDTH-1]}}, in_mat2 };  // Extend in_mat2
 
-//I believe we're good to delete the below work. From my understanding the RTL generation will
-//create something, either a standard cell or bunch of logic gates, to handle our functional code.
-/*
- We're unsure if there is a standard cell that can handle multiplication directly. If there is then our current code is good.
- Otherwise we will use the following code to build a multiplier from logic gates. 
+        // Perform the 8-bit by 8-bit multiplication to get a 16-bit result
+        full_product = in_mat1_ext * in_mat2_ext;
 
+        // Truncate to the lower 8 bits of the 16-bit result for the final product
+        out_mat = full_product[DATA_WIDTH*2-1:0];
+    end
 
-module matrix_multiplication #(
-    parameter DATA_WIDTH = 4
-)
-( 
-    input wire [DATA_WIDTH-1:0] in_elem1,
-    input wire [DATA_WIDTH-1:0] in_elem2,
-    output reg [DATA_WIDTH+3:0] out_elem
-);
-wire [DATA_WIDTH+3:0] mult_result;
-
-// Instantiate the matrix_multiplication_module
-matrix_multiplication_module mult_block(
-    .A(in_mat1),
-    .B(in_mat2),
-    .P(mult_result)
-);
-
-    // Use an always block to drive the output based on the result
-always@(*) begin
-    out_mat = mult_result;
-end
-  
-endmodule
-
-
-*/
+endmodule 
