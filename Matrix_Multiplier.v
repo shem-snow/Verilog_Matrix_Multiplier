609215d745a58ff7c5b001ce5bbec058b0487cd6
Shem <shem-snow@hotmail.com>
2024-10-31 16:35:11 -0600

    edited the top level to include a status register

--- Matrix_Multiplier.v
+++ Matrix_Multiplier.v
@@ -2,8 +2,8 @@
 	This is our top-level module
 */
 module Matrix_Multiplier(
-	input [11:0] matrix_A,
-	input [11:0] matrix_B,
+	input [15:0] matrix_A,
+	input [15:0] matrix_B,
 	input clock,
 	input start,
 	input reset,
@@ -15,8 +15,9 @@ module Matrix_Multiplier(
 // Internal wires
 wire [3:0] element_A;
 wire [3:0] element_B;
-wire [2:0] AB_entries;
+wire [3:0] AB_entries;
 wire write;
+wire multiply_matrix;
 wire [7:0] multiply_out;
 wire update_regfile;
 wire [2:0] address_regfile;
@@ -26,11 +27,12 @@ wire [31:0] adder_result;
 wire addition_done;
 
 // Instantiate all the modules
-FSM fsm(.clock(clock), .start(start), .reset(reset), .entry_count(AB_entries), .load_matrix(write), .add(start_add), .done(addition_done));
-MatrixA mata(.matrixA(matrix_A), .entry_out(AB_entries), .element(element_A));
-MatrixB matb(.matrixB(matrix_B), .entry_out(AB_entries), .element(element_B));
-matrix_multiplication multiplier(.in_mat1(element_A), .in_mat2(element_B), .out_mat(multiply_out));
-RegFile regfile(.reset(reset), .product_in(mutliply_out), .reg_specifier(address_regfile), .update_reg(update_regfile), .contents(regfile_contents)); // TODO:
+FSM fsm(.clock(clock), .start(start), .reset(reset), .entry_count(AB_entries),.multiply_matrix(multiply_matrix) ,.load_matrix(write), .add(start_add), .done(addition_done));
+status_reg statusREG(.clk(clock),.reset(reset),.multiply(multiply_matrix),.entry_out(AB_entries));
+MatrixA mata(.clk(clock),.matrixA(matrix_A), .entry_out(AB_entries), .element(element_A));
+MatrixB matb(.clk(clock),.matrixB(matrix_B), .entry_out(AB_entries), .element(element_B));
+Multiplier multiplier(.in_mat1(element_A), .in_mat2(element_B), .out_mat(multiply_out));
+RegFile regfile(.reset(reset),.clk(clock), .product_in(multiply_out), .reg_specifier(AB_entries), .update_reg(write), .contents(regfile_contents)); // TODO:
 matrix_addition accumulator(.matrix(regfile_contents), .matrix_result(adder_result));
 result_handler result_handler(.D(adder_result), .reset(reset), .done(addition_done), .Q(matrix_result), .address(matrix_count));
 
